1bb6c71d5f61d1b9c7515243f843c8fe4f01b819
brandaoplaster <brandaoplaster@gmail.com>
2026-04-28 17:24:44 -0300

    feat: Add parameter sort for order contact list

--- shared/contact_loader.rb
+++ shared/contact_loader.rb
@@ -2,11 +2,11 @@ require "json"
 require_relative "contact"
 
 class ContactLoader
-  def self.load(file_path, count)
+  def self.load(file_path, count, sort = true)
     if File.exist?(file_path)
       load_from_file(file_path)
     else
-      generate_and_save(file_path, count)
+      generate_and_save(file_path, count, sort)
     end
   end
 
@@ -17,10 +17,10 @@ class ContactLoader
     data.map { |c| Contact.new(c["name"], c["email"], c["phone"]) }
   end
 
-  def self.generate_and_save(file_path, count)
+  def self.generate_and_save(file_path, count, sort)
     contacts = Contact.generate_list(count)
     contacts << Contact.new("John Doe", "", "")
-    contacts.sort_by!(&:name)
+    contacts.sort_by!(&:name) if sort
 
     data = contacts.map { |c| { name: c.name, email: c.email, phone: c.phone } }
     File.write(file_path, data.to_json)
